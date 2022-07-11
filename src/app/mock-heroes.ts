1e470cd6a1eb8c38544ce7232279b3106c1e37ff
Claude Mokbel <claudemokbel@gmail.com>
2022-07-11 14:56:20 -0700

    display h

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+ {id: 12, name: "Dr. Strange"},
+ {id: 13, name: "Thor"},
+ {id: 14, name: "Captain America"},
+ {id: 15, name: "Hulk"},
+ {id: 16, name: "Hawkeye"},
+ {id: 17, name: "Storm"},
+ {id: 18, name: "Mystique"},
+ {id: 19, name: "Wolverine"},
+ {id: 20, name: "Spiderman"}   
+]
